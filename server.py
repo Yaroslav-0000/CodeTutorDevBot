60174081469bb8433f078d46450249301371008b
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-02-05 18:39:40 +0200

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("""
-Я бот созданный для обучения программированию, вот что я вам могу предложу:
+Я бот созданный для обучения программированию, вот что я вам могу предложить:
 
 python⬇️
 написать текст /exemple_python_print
